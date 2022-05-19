dc6d6844f51c1a44492466f651dcc0987eee5244
alperkrtglu <alperts95@gmail.com>
2022-05-20 02:17:53 +0300

    COMMIT-1

---
+++ src/main/java/com/example/emailverification/user/User.java
@@ -0,0 +1,70 @@
+package com.example.emailverification.user;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "USER")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String username;
+    private String password;
+    private String email;
+
+    @Enumerated(value = EnumType.STRING)
+    private UserRole role;
+
+    public User(String name, String username, String password, String email, UserRole role) {
+        this.name = name;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
